2010b27fe6c689b4677d67d54f7ba4988689cd54
Gustavo Dias de Oliveira <gust.dias@gmail.com>
2018-11-27 11:42:07 -0200

    Property model creation.

---
+++ models/Property.js
@@ -0,0 +1,28 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Property Model
+ * =============
+ */
+
+var Property = new keystone.List('Property');
+
+Property.add({
+	name: { type: String, required: true, index: true },
+	phones: { type: Types.TextArray },
+	propType: { type: Types.Select, options: [
+		{ value: 'house', label: 'House' },
+		{ value: 'appartment', label: 'Appartment' },
+		{ value: 'townhouse', label: 'Townhouse' },
+		{ value: 'other', label: 'Other...' },
+	] },
+	details: { type: Types.Markdown, default: '# My property :D' },
+	createdAt: { type: Date, default: Date.now },
+	thumbPhoto: { type: Types.CloudinaryImage },
+	photos: { type: Types.CloudinaryImages },
+});
+
+Property.defaultSort = '-createdAt';
+Property.defaultColumns = 'name, propType, createdAt, thumbPhoto';
+Property.register();
