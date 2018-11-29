04766ff095037ebd2e3dcea4d404c38539ea5584
Gustavo Dias de Oliveira <gust.dias@gmail.com>
2018-11-29 10:44:36 -0200

    Filter properties query implementation. Display property type on listing.

--- routes/views/properties.js
+++ routes/views/properties.js
@@ -9,7 +9,38 @@ exports = module.exports = function (req, res) {
 	locals.section = 'Properties';
 
 	// Load the properties by sortOrder
-	view.query('properties', keystone.list('Property').model.find().sort('sortOrder'));
+	var propertiesQuery = keystone.list('Property').model.find().sort('sortOrder');
+	if (req.query.name) {
+		propertiesQuery = propertiesQuery.where('name', req.query.name);
+	}
+	if (req.query.minPrice) {
+		propertiesQuery = propertiesQuery.where('price').gte(+req.query.minPrice);
+	}
+	if (req.query.maxPrice) {
+		propertiesQuery = propertiesQuery.where('price').lte(+req.query.maxPrice);
+	}
+	if (req.query.type) {
+		propertiesQuery = propertiesQuery.where('propType', req.query.type);
+	}
+	var areaType = 'usefulArea';
+	switch (req.query.areaType) {
+		case 'useful':
+			areaType = 'usefulArea';
+			break;
+		case 'total':
+			areaType = 'totalArea';
+			break;
+	}
+	if (req.query.minArea) {
+		propertiesQuery = propertiesQuery.where(areaType).gte(+req.query.minArea);
+	}
+	if (req.query.maxArea) {
+		propertiesQuery = propertiesQuery.where(areaType).lte(+req.query.maxArea);
+	}
+	if (req.query.minRooms) {
+		propertiesQuery = propertiesQuery.where('rooms').gte(+req.query.minRooms);
+	}
+	view.query('properties', propertiesQuery);
 
 	// Render the view
 	view.render('property');
